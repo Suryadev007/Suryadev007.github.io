1cca20777714fad16fd565a3a5d01b2d29cee98a
Suryadev007 <107113316+Suryadev007@users.noreply.github.com>
2025-11-04 13:54:45 +0530

    new portfolio

---
+++ Data.js
@@ -0,0 +1,104 @@
+// Yeh file ek simple JS object export karegi
+const Data = {
+  UserName: "Surya Dev",
+  UserPhone: "+91-9877528394",
+  UserEmail: "suryadev7056@gmail.com",
+  UserLinkedin: "https://www.linkedin.com/in/surya-d-44b4ba33a",
+  UserGit: "https://github.com/Suryadev007",
+  UserInfo: `Hi, I’m Surya Dev, a passionate Web Developer skilled in building modern, responsive, and user-friendly websites. I specialize in frontend development using technologies like React, HTML, CSS, and JavaScript, and I’m also experienced with CMS platforms such as WordPress and Shopify.
+I enjoy turning ideas into functional and visually appealing digital experiences that help businesses grow and users engage effortlessly.`,
+  BasicInfo:
+    "I’m Surya Dev, a web developer, with knowledge of CMS like wordpress & shopify.",
+  expertize: {
+    frontend: ["React", "HTML", "CSS", "JavaScript"],
+    backend: ["PHP, MongoDB, SQl"],
+    cms: ["WordPress", "Shopify", "WIX"],
+    tools: ["Git", "VS Code", "Figma"],
+    "page builder": ["Elementor", "DIVI"],
+  },
+  Experience: [
+    ["TagHash Media", "Web Developer", "Feb,2025", "Present"],
+    ["SainSoft", "Web Developer", "Aug,2024", "Jan,2025"],
+    ["STEP-GNDEC", "Full Stack Developer Internship", "Jan,2024", "Jun,2025"],
+  ],
+
+  Education: [
+    [
+      "Guru Nanak Dev Engg College",
+      "Sept 2022 to June 2024",
+      "Master In Computer Applications",
+    ],
+    [
+      "PCTE Group of Institutes",
+      "July 2019 to June 2022",
+      "Bachelor In Computer Applications",
+    ],
+  ],
+
+  ProjectLists: [
+    [
+      "images/portfolio/lamp.jpg",
+      "Caddilac Uniform",
+      "Wordpress",
+      "https://www.cadillacuniform.com/",
+    ],
+    [
+      "images/portfolio/lamp.jpg",
+      "RTD Cleaning",
+      "Wordpress",
+      "https://rtdcommclean.com.au/",
+    ],
+    ["images/portfolio/lamp.jpg", "Vgass", "Shopify", "#"],
+    [
+      "images/portfolio/lamp.jpg",
+      "Pax Scrubs",
+      "Shopify",
+      "https://paxscrubs.com/",
+    ],
+    [
+      "images/portfolio/lamp.jpg",
+      "Prytec Solutions",
+      "Wordpress",
+      "https://prytec.com.au/",
+    ],
+    [
+      "images/portfolio/lamp.jpg",
+      "HM StripOuts & Defits",
+      "Wordpress",
+      "https://hmstripouts.com.au/",
+    ],
+    ["images/portfolio/lamp.jpg", "ISTN", "Wordpress", "https://istn.com/"],
+    [
+      "images/portfolio/lamp.jpg",
+      "Circle Of Life Health",
+      "Wordpress",
+      "https://colhealth.com.au/",
+    ],
+    [
+      "images/portfolio/lamp.jpg",
+      "Clear Vuem",
+      "Wordpress",
+      "https://clearvuemn.com/",
+    ],
+    [
+      "images/portfolio/lamp.jpg",
+      "Aaron’s Custom Plumbing",
+      "Wordpress",
+      "https://aaronscustomplumbing.com/",
+    ],
+    [
+      "images/portfolio/lamp.jpg",
+      "Atkins",
+      "Wordpress",
+      "https://atkinsplumbingandgas.com.au/",
+    ],
+    [
+      "images/portfolio/lamp.jpg",
+      "AP Plumbing",
+      "Wordpress",
+      "https://adrianperezplumbing.com/",
+    ],
+  ],
+
+  CV_File: "cv.pdf",
+};
